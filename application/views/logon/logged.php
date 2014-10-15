6321be772c4880be8588be60b3eb215b55ebe832
Miguel Rueda <askmeyou15@hotmail.com>
2014-10-15 15:22:39 -0500

    logged - estilos en css

--- application/views/logon/logged.php
+++ application/views/logon/logged.php
@@ -1,45 +1,14 @@
-<style type="text/css">
-    #logged_data .img_logged{
-        width: 62px;
-        height: 62px;
-        border: 3px solid #FFFFFF;
-    }
-    #logged_data .data_area{
-        /*width: auto; */
-        text-align: left; 
-        margin-top: 4px;   
-        float: right;
-    }
-    #logged_data .btn_logout{
-        padding: 11px 0 0 0;
-    }
-    #logged_data .texto_normal{
-        color: #7e7e7e;
-    }
-    #logged_data .texto_negrita{
-        color: #575757;
-    }
-    #logged_data .texto_bienvenida {
-        font-size: 20px;
-    }   
-    a:visited,
-    a:link {   
-        text-decoration:none;
-        color: #868686;
-    }
-</style>
-
+<link rel="stylesheet" href="<?php echo base_url('application/assets/css/logon/logged.css'); ?>">
 <div>
-    <div style="float:right; max-width: 85%; padding: 7px 0 0 4px;">
+    <div id="bienvenidalabel">
         <span class="texto_negrita texto_bienvenida">Bienvenido...!!!</span>
-        <a href="<?php echo site_url('logon/logout'); ?>" data-toggle="tooltip" data-placement="bottom" title="Cerrar sesión" style="float: right; margin: -15px 0 0 10px;">
+        <a class="logoutbtn" href="<?php echo site_url('logon/logout'); ?>" data-toggle="tooltip" data-placement="bottom" title="Cerrar sesión">
             <i class="fa fa-sign-out fa-3x"></i>
         </a>        
         <br/>
-        <span class="texto_normal"><?php echo $name; ?></span>       
+        <span class="texto_normal"><?php echo $name; ?></span>
     </div>    
-    <div style="float:right;">
+    <div id="imguserlogin">
         <img src="<?php echo base_url('application/assets/images/logon/logged.png'); ?>" alt="Sesión iniciada" class="img-circle img_logged" />
-    </div>
-    
+    </div>    
 </div>
