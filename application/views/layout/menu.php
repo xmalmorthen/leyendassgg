2b6c65fc970cb75673fd39301824190079d77251
Miguel Rueda <askmeyou15@hotmail.com>
2014-10-17 15:56:14 -0500

    modificación de observaciones

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="<?php echo base_url(ASSETS .'css/layout/menu.css'); ?>">
 <div id="generalmenuoptions" class="row">
   <div class="col-md-5 col-md-offset-7">
-    <a href="<?php echo site_url('leyenda/history'); ?>" role="button" class="btn btn-success btn-lg colorbtnmnu" data-toggle="tooltip" data-placement="bottom" title="Historial de Leyendas"><i class="fa fa-table fa-2x"></i> Históricos</a>
+    <a href="<?php echo site_url('leyenda/history'); ?>" role="button" class="btn btn-success btn-lg colorbtnmnu" data-toggle="tooltip" data-placement="bottom" title="Historial de Leyendas"><i class="fa fa-table fa-2x"></i> Histórico</a>
     <a href="<?php echo site_url('leyenda'); ?>" role="button" class="btn btn-warning btn-lg colorbtnmnu" data-toggle="tooltip" data-placement="bottom" title="Actualizar Leyenda"><i class="fa fa-file-text fa-2x"></i> Actualizar</a>    
  </div>
 </div>
