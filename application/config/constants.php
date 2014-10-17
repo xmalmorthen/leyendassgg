60afb380cb2f758caaf956520271b09759a01395
Miguel Rueda <askmeyou15@hotmail.com>
2014-10-17 10:29:55 -0500

    acomodando rol de inicio de sesión

--- application/config/constants.php
+++ application/config/constants.php
@@ -37,7 +37,7 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 define("ASSETS", "assets/");
-define("ROL_LOGEO", "3");
+define("ROL_LOGEO", "4");
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
