5d45440cc19bb7e64ec9edda24f3c87d5a74784b
Miguel Rueda <askmeyou15@hotmail.com>
2014-10-17 15:45:53 -0500

    ajustes de observaciones

--- application/views/leyenda/index.php
+++ application/views/leyenda/index.php
@@ -101,19 +101,19 @@
                 <span>Pantalla para actualizar la leyenda.</span>
                 <span class="label label-danger tamaniolabel">Nota:</span> <b>Únicamente se permite actualizar la leyenda actual...</b>
                 <br/><br/>
-                La primer parte del formulario muestra la <span class="label label-success tamaniolabel">leyenda actual</span> que se utiliza en los sistemas para los formatos de pie de páginas, 
-                en la parte de <span class="label label-success tamaniolabel"><i class="fa fa-info-circle fa-1x"></i> [ Ver más información... ]</span> podrá consultar los datos complementarios a la leyenda actual como son
-                el <b>año</b>, <b>número de decreto</b>, <b>fecha de decreto</b> y <b>fecha de registro</b>.
+                En la primer parte de la pantalla se muestra la <span class="label label-success tamaniolabel">leyenda actual</span> la cual se utiliza en los sistemas para los formatos de pie de páginas en documentos,
+                en la opción de <span class="label label-success tamaniolabel"><i class="fa fa-info-circle fa-1x"></i> [ Ver detalle. ]</span> podrá consultar los datos complementarios a la leyenda actual como son
+                el <b>Año</b>, <b>Número de decreto</b>, <b>Fecha de decreto</b> y <b>Fecha de registro</b>.
                 <br/><br/>
-                La siguiente parte del formulario contiene los campos necesarios para la actualización de la leyenda. El símbolo <span class="label label-danger tamaniolabel"><i class="fa fa-asterisk"></i></span>
+                La siguiente parte contiene los campos necesarios para la actualización de la leyenda. El símbolo <span class="label label-danger tamaniolabel"><i class="fa fa-asterisk"></i></span>
                 indica que es un dato requerido para que la actualización se realice con éxito. Los campos necesarios son los siguientes:
                 <br/>
-                <span class="label label-default tamaniolabel">Número de Decreto</span> se refiere al dato identificativo con el cual se decretó la leyenda,
-                <span class="label label-default tamaniolabel">Fecha de Decreto</span> se requiere especificar la fecha en que fue decretada la leyenda,
-                <span class="label label-default tamaniolabel">Leyenda</span> en éste campo es necesario escribir la cadena alfanumérica que se utilizará como la nueva leyenda.
+                <span class="label label-default tamaniolabel">Número de Decreto</span> indica el número con el cual se identifica el decretó de la leyenda,
+                <span class="label label-default tamaniolabel">Fecha de Decreto</span> se refiere a la fecha en que se decidió registrar la leyenda,
+                <span class="label label-default tamaniolabel">Leyenda</span> en éste campo es necesario escribir el texto que se utilizará como nueva leyenda para el píe de página.
                 <br/><br/>
-                En la parte inferior del formulario se encuentra el botón <span class="label label-success tamaniolabel">Guardar</span> el cuál, una vez capturada la información requerida, nos permitirá actualizar los cambios y tomarlos como permanentes hasta la próxima actualización.
-                El botón de <span class="label label-danger tamaniolabel">Limpiar</span> nos ayuda a reiniciar los contenedores de información a su estado inicial al momento de abrir el formulario.
+                En la parte inferior del formulario se encuentran los botones de <span class="label label-success tamaniolabel">Guardar</span> y <span class="label label-danger tamaniolabel">Limpiar</span>
+                en donde <strong>Guardar</strong> se utilizará para almacenar los cambios realizados a la leyenda y el de <strong>Limpiar</strong> para eliminar los datos ingresados en los campos.
             </div>
         </div>
     </div>
